6e0d82acd338a2d5926552f2079df17f092ff470
nschneider18 <schnei54@purdue.edu>
2016-11-30 15:51:06 -0500

    Connected the Server to the MongoInterface
    I connected the Javascript to the MongoDB so they can connect to each
    other.

--- src/helperFunctions/mongoInterface.js
+++ src/helperFunctions/mongoInterface.js
@@ -1,9 +1,38 @@
-var mongoDB = require('mongodb');
+var mongo = require('mongodb');
+
+var Server = mongo.Server,
+    Db = mongo.Db,
+    BSON = mongo.BSONPure;
+
+var server = new Server('localhost', 9000, {auto_reconnect: true});
+db = new Db('Wikirace', server, {safe: true});
+
+db.open(function(err, db) {
+    if(!err) {
+        console.log("Connected to database");
+        db.collection('Wikirace', {safe:true}, function(err, collection) {
+            if (err) {
+                console.log("The 'wines' collection doesn't exist");
+            }
+	    else{
+		console.log("There was no error");
+	    }
+        });
+    }
+});
 
 exports.findAll = function(req, res) {
-    res.send([{name:'player1'}, {name:'player2'}, {name:'player3'}]);
+    db.collection('Wikirace', function(err, collection) {
+        collection.find().toArray(function(err, items) {
+            res.send(items);
+        });
+    });
 };
 
 exports.findById = function(req, res) {
     res.send({id:req.params.id, name: "The Name", description: "description"});
+};
+
+exports.findByIdName = function(req, res) {
+    res.send({id: req.params.id, name: req.params.name, description: "description"});
 };
--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ var app = express();
 
 app.get('/players', mongoInterface.findAll);
 app.get('/players/:id', mongoInterface.findById);
+app.get('/players/:id/:name', mongoInterface.findByIdName);
+
 
 app.listen(3000);
 console.log('Listening on port 3000...');
