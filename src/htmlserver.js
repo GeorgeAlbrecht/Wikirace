5d1cd2072fc863035f7707122a3540de6f0bd95f
davis650 <davis650@purdue.edu>
2016-12-04 15:01:04 -0500

    Added login and sign up page. No logic for u-name/pw checking yet.

--- src/htmlserver.js
+++ src/htmlserver.js
@@ -26,22 +26,58 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-	res.render('index');
+	res.render('login');
 })
+
+app.post("/game", (req,res) => {
+	console.log("in game POST call");
+	console.log(req.body);
+	//checking the database for username and password combo
+	// if successful, return index
+	// if failed, return error page with button back to login
+	res.render('index');
+});
+
+app.post("/sign-up", (req,res) => {
+	// just a post to utilize a button to render another page
+	console.log("in sign-up POST call");
+	res.render('sign_up');
+});
+
+app.post("/signed-up", (req,res) => {
+	console.log("in signed-up post call");
+	console.log(req.body);
+	res.render('login');
+});
 
 app.post("/new-game", (req, res) => {
 	res.render('newGame');
 })
 
+/*app.get("/race", (req, res) => {
+	console.log(req.body);
+	res.render('race', {
+		_mode: req.body.game_mode,
+		_start: req.body.start,
+		_dest: req.body._dest
+	});
+});*/
+
 app.post("/start-dest", (req, res) => {
+	console.log("in start-dest POST call");
 	console.log(req.body);
 
-	res.render('startdest');
+	res.render('startdest', {_mode: req.body.game_mode});
 });
 
 app.post("/race", (req, res) => {
+	console.log("in race POST call");
 	console.log(req.body);
-	res.render('race');
+	res.render('race', {
+		_mode: req.body._game_mode,
+		_start: req.body.start,
+		_dest: req.body.dest
+	});
 });
 
 app.get("/wiki/:name", (req, res) => {
